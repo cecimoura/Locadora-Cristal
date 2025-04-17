d41897590e324a6914a2214eddf2bf18dde7879c
cecimoura <cecimoura91@gmail.com>
2025-04-17 13:19:45 -0300

    Entity cliente ok

--- src/main/java/org/example/Entities/ClienteEntity.java
+++ src/main/java/org/example/Entities/ClienteEntity.java
@@ -4,8 +4,42 @@ import javax.persistence.*;
 
 @Entity
 @Table(name = "tb_cliente")
+
 public class ClienteEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    @Column(nullable = false, length = 200)
+    private String nome;
+
+    @Column(nullable = false, unique = true, length = 11)
+    private String cpf;
+
+    @Column(length = 20)
+    private String telefone;
+
+    //construtores
+    public ClienteEntity(){}
+
+    //construtor parametrizado
+    public ClienteEntity(String nome, String cpf, String telefone){
+        this.nome = nome;
+        this.cpf = cpf;
+        this.telefone = telefone;
+    }
+
+    //getters e setter
+
+    public Long getId() {return id;}
+    public void setId(long id) {this.id = id;}
+
+    public String getNome() {return nome;}
+    public void setNome(String nome) {this.nome = nome;}
+
+    public String getCpf() {return cpf;}
+    public void setCpf(String cpf) {this.cpf = cpf;}
+
+    public String getTelefone() {return telefone;}
+    public void setTelefone(String telefone) {this.telefone = telefone;}
 }
